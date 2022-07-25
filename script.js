95a59c7e4a64af184418628fea7af2bffab8f300
Geek-Tekina <tpbc1717@gmail.com>
2022-07-25 10:47:19 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function upload() {
+    const progressBar = document.querySelector('.progress-bar');
+    progressBar.setAttribute('id','play-animation');
+  }
+  function cloudupload(){
+    const progressBar =document.querySelector('.progress-bar');
+    progressBar.setAttribute('id','play-cloud');
+  }
